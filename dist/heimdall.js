b0e7e5c2894d63057a80d08fd8ea3d993a4b5834
mizuki_r <ry.mizuki@gmail.com>
2013-12-10 19:56:44 +0900

    issues #1 fixed

--- dist/heimdall.js
+++ dist/heimdall.js
@@ -1,4 +1,4 @@
-/*! Heimdall - v0.0.1 - 2013-12-10
+/*! Heimdall - v0.0.2 - 2013-12-10
 * https://github.com/mizuki/Heimdall
 * Copyright (c) 2013 mizuki_r; Licensed MIT */
 var Heimdall = (function () {
